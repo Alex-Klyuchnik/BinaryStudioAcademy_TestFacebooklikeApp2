1603f6e94c834e85a4d20b5638558a17329992c1
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-18 21:04:44 +0300

    Created base controller

---
+++ BinaryStudioAcademy_TestFacebooklikeApp2/Infrastructure/BaseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BinaryStudioAcademy_TestFacebooklikeApp2.Infrastructure
+{
+    public class BaseController :Controller
+    {
+        protected override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (!string.IsNullOrEmpty(SimpleSessionPersister.Username))
+            {
+                filterContext.HttpContext.User = 
+                    new CustomPrincipal(
+                        new CustomIdentity(
+                            SimpleSessionPersister.Username));
+            }
+
+            base.OnAuthorization(filterContext);
+        }
+    }
+
+    public static class SimpleSessionPersister
+    {
+        private static string usernameSessionVar = "username";
+        public static string Username
+        {
+            get
+            {
+                if (HttpContext.Current == null) return string.Empty;
+                var sessionVar =
+                    HttpContext.Current.Session[usernameSessionVar];
+                if (sessionVar != null)
+                {
+                    return sessionVar as string;
+                }
+                return null;
+            }
+            set { HttpContext.Current.Session[usernameSessionVar] = value; }
+        }
+    }
+}
