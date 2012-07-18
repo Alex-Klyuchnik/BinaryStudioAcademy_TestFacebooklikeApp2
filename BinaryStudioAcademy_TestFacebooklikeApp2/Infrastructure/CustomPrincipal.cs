a6dfc88e8adbbde7725161515ba26436a45b1443
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-18 20:51:07 +0300

    Custom principal implementation

---
+++ BinaryStudioAcademy_TestFacebooklikeApp2/Infrastructure/CustomPrincipal.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Principal;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BinaryStudioAcademy_TestFacebooklikeApp2.Infrastructure
+{
+    public class CustomPrincipal : IPrincipal
+    {
+        public CustomPrincipal(CustomIdentity identity)
+        {
+            this.Identity = identity;
+        }
+
+        public IIdentity Identity { get; private set; }
+
+        public bool IsInRole(string role)
+        {
+            return true;
+        }
+    }
+}
