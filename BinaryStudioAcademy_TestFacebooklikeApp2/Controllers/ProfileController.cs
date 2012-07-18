f27ee5e0787f1ca84fce9eb4cc24eceef237a14b
Alex Klyuchnik <alex.aquasky2@gmail.com>
2012-07-18 21:23:59 +0300

    Profile controller implementation

---
+++ BinaryStudioAcademy_TestFacebooklikeApp2/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinaryStudioAcademy_TestFacebooklikeApp2.Infrastructure;
+using BinaryStudioAcademy_TestFacebooklikeApp2.Models;
+
+namespace BinaryStudioAcademy_TestFacebooklikeApp2.Controllers
+{
+    public class ProfileController : BaseController
+    {
+        //
+        // GET: /Profile/
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(new LoginModel{Email = this.User.Identity.Name});
+        }
+
+    }
+}
